6cf9c11b8780692085c695657bb210a096b4ac4d
Evelyn Arias <eve.arias91@gmail.com>
2022-11-21 17:20:34 -0300

    deploy

--- src/main/java/com/example/appEjemplo/controller/LibroController.java
+++ src/main/java/com/example/appEjemplo/controller/LibroController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class LibroController {
 
     @Autowired
